0da14eee34e6202e7869db00ed9b1eb37b2c361c
Ramchel Ortiz <ram.ortizb@gmail.com>
2023-04-15 16:44:54 -0600

    Posee el reporte 1 y 2, sin documentación.

--- códigoModificado.py
+++ códigoModificado.py
@@ -3,6 +3,7 @@
 from unidecode import unidecode
 import datetime
 import speech_recognition as sr
+from os import system
 
 #Variables Globales---------------------------------------------------------------------------------------------------------
 
@@ -11,14 +12,19 @@ registroApartados=[]
 registroApartadosFinal=[]
 registroPuntos=[]
 registroVoz=[]
+codigo=1
 codigoVoz=1
 opciones=["La misma persona continua hablando","Otro miembro toma la palabra","El punto de la agenda a terminado"]
+recuento=[]
+registroReportes=["Reporte1","Reporte2","Reporte3"]
 #-------------------------------------------------------------------------------------------------------------------------
-
 
 #Sección de agenda---------------------------------------------------------------------------------------------------------
 def agregarParticipantes():
-    print("Se procede a realizar el registro de participantes\n")
+    """_summary_
+    
+    """
+    print("\n Se procede a realizar el registro de participantes\n")
     
     seguirPreguntando=1
     posicionDondeAgregar=0
@@ -56,12 +62,10 @@ def agregarParticipantes():
                 break
             else:
                 print("Por favor, responda con 'si' o 'no'.\n")             
-    
-    return(print(registroParcicipantes))
 
 
 def agregarCodigoapartado(apartados:list):
-    codigo = 1
+    global codigo
     for i in apartados:
         codigoStr = str(codigo)
         registroApartadosFinal.append([codigoStr, i])
@@ -106,7 +110,7 @@ def agregarPuntos(codigo:str):
                         print("Por favor, ingrese un punto que no sea solo un número.\n") 
         
             elif unidecode(respuesta.lower()) == "no":
-                print("\nEntendido no se agregaán más puntos a este apartado")
+                print("\nEntendido no se agregarán más puntos a este apartado\n")
                 seguirPreguntando+=1
                 break
             else:
@@ -116,11 +120,12 @@ def masApartados():
     while True:
             respuesta = input('¿Desea ingresar otro apartado? Digite "si" si desea hacerlo digite "no" si no lo desea hacer: ')
             if unidecode(respuesta.lower()) == "si":
+                registroApartados.clear()
                 agregarApartados()
                 agregarPuntos(agregarCodigoapartado(registroApartados))
         
             elif unidecode(respuesta.lower()) == "no":
-                print("\nEntendido no se agregaán más puntos a este apartado")
+                print("\nEntendido no se agregarán más puntos a este apartado")
                 break
             else:
                 print("Por favor, responda con 'si' o 'no'.\n") 
@@ -129,15 +134,14 @@ def masApartados():
  #Sección de reconociminto de voz----------------------------------------------------------------------------------------
  
 def apartadoDeseado():
-    for i, apartado in enumerate(registroApartados):
-        print(str(i) + ": " + apartado)
+    for i, apartado in enumerate(registroApartadosFinal):
+        print(str(i) + ": " + apartado[1])
     
     while True:
         try:
-            indice=int(input("Ingrese el índice correspondiente al apartado al que desea ingresar o -1 si no desea ingresar más: "))
-            if indice == -1:
-                break#Eliminar este -1, simpre tiene que existir 
-            nombre = registroApartados[indice]
+            indice=int(input("Ingrese el índice correspondiente al apartado al que desea ingresar: "))
+            print("\n")
+            nombre = registroApartadosFinal[indice][1]
             return nombre
                 
         except:
@@ -179,12 +183,13 @@ def generarClaves():
 
 
 def obtenerParticipante():
+    print("\n")
     for i, participante in enumerate(registroParcicipantes):
         print(str(i) + ": " + participante)
         
     while True:
         try:
-            indice = int(input("Indica quien participantes escribiendo tu ínidice correspondiente: "))
+            indice = int(input("Indica quien participantes escribiendo tu índice correspondiente: "))
             participante = registroParcicipantes[indice]
             break
         except:
@@ -237,16 +242,6 @@ def reconocimientoSinParticipanteAgenda(clavePunto,claveApartado,participante):
     registroVoz.append([str(codigoVoz),participante,claveApartado,clavePunto,texto,fecha])
     codigoVoz+=1
 
-def maximoIntervenciones(clavePunto,claveApartado,participante):
-    valor=True
-    contador=0
-    for i in registroVoz:
-      if [1]==participante and [2]==claveApartado and [3]==clavePunto:
-          contador+=1
-    if contador == 2:
-        valor=False
-        return valor 
-
 def siguienteAccion(claves):
         clavePunto=claves[0]
         claveApartado=claves[1]
@@ -254,19 +249,19 @@ def siguienteAccion(claves):
         siguePreguntando=1
         while siguePreguntando==1:   
             while True:
-                print("\n")
+                print("\n Opciones: \n")
                 for i, opcion in enumerate(opciones):
                     print(str(i) + ": " + opcion)
                 try:
-                    respuesta= int(input("Indica la acción que desea realizar mediante el índice corespondiente: "))                  
+                    respuesta= int(input("\nIndica la acción que desea realizar mediante el índice corespondiente: "))                  
                     if respuesta==0: #Signica que "Sigue hablando la misma persona" 
                         reconocimientoSinParticipanteAgenda(clavePunto,claveApartado,participante)
                         break
                     elif respuesta==1:  #Significa que Otro miembro toma la palabra
-                        reconocimientoSinAgenda(clavePunto,claveApartado,participante)
+                        reconocimientoSinAgenda(clavePunto,claveApartado)
                         break
                     elif respuesta==2: #Significa que "El punto de la agenda a terminado"
-                        print("Entendido, este punto de la agenda ha sido discutido, favor indicar nuevo punto\n")
+                        print("\nEntendido, este punto de la agenda ha sido discutido.\n")
                         siguePreguntando+=1
                         break
                     else:
@@ -281,7 +276,7 @@ def seguirReconociendo():
             try:
                 respuesta = input('¿Desea terminar el reconocimiento? Digite "si" si desea hacerlo digite "no" si no lo desea hacer: ')
                 if unidecode(respuesta.lower()) == "si":
-                    print("Entendido, el reconocimiento ha terminado\n")
+                    print("\nEntendido, el reconocimiento ha terminado\n")
                     seguirPreguntando+=1
                     break                
                 elif unidecode(respuesta.lower()) == "no":
@@ -296,15 +291,132 @@ def seguirReconociendo():
 
 #Sección de reportes-----------------------------------------------------------------------------------------------------------------------------------------
 
+#Menú reportes:
+def menuReportes():
+        print("\n")
+        for i, opcion in enumerate(registroReportes):
+            print(f"{i+1}: {opcion}")
+                
+        while True:
+            try:
+                indice=int(input("\nIngrese el índice correspondiente al reporte que desea realizar: "))
+                if indice==1:
+                    print("\n Se realizará el reporte 1\n")
+                    primerReporte()
+                    break
+                elif indice==2:
+                    print("\n Se realizará el reporte 2\n")
+                    segundoReporte()
+                    break
+                elif indice==3:
+                    print("\n Se realizará el reporte 3\n")
+                    tercerReporte()
+                    break
+                else:
+                    raise ValueError
+            except:
+                print("El índice indicado no es válido.")    
+        
+def seguirReportes():
+    seguirPreguntando=1
+    while seguirPreguntando==1:
+        while True:
+                try:
+                    respuesta = input('¿Desea terminar de imprimir reportes? Digite "si" si desea hacerlo digite "no" si no lo desea hacer: ')
+                    if unidecode(respuesta.lower()) == "si":
+                        print("\nEntendido, no se imprimen más reportes\n")
+                        seguirPreguntando+=1
+                        break                
+                    elif unidecode(respuesta.lower()) == "no":
+                        menuReportes()
+                    else:
+                        raise ValueError 
+                except:
+                    print('Responda con "si" o "no" únicamente')
+    #-----------------------------------------------------------------------------------------------------------------------------------------------------------
+#Primer reporte
+
+def imprimir_voz(apartados, puntos,voz):
+    for apartado in apartados:
+        print(apartado[1] + ":")
+        for punto in puntos:
+            if punto[2] == apartado[0]:
+                print("\t" + punto[1] + ":")
+                for v in voz:
+                    if v[2] == apartado[0] and v[3] == punto[0]:
+                        print("\t\t" + v[1] + " " + v[5]+ ": "  + " " + v[4])
+
+def primerReporte():
+    imprimir_voz(registroApartadosFinal,registroPuntos,registroVoz)
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Segundo reporte--------------------------------------------------------------------------------------------------------------------------------------------
+def obtenerRecuentoPalabras(personas, registro):
+    global recuento
+    for persona in personas:
+        palabras_dichas = 0
+        for registro_persona in registro:
+            if registro_persona[1] == persona:
+                palabras_dichas += len(registro_persona[4].split())
+        # Buscamos si ya existe un elemento en recuento que corresponda a esta persona
+        existe_persona = False
+        for i in range(len(recuento)):
+            if recuento[i][0] == persona:
+                recuento[i][1] += palabras_dichas
+                existe_persona = True
+                break
+        # Si no encontramos la persona en recuento, la agregamos como un nuevo elemento
+        if not existe_persona:
+            recuento.append([persona, palabras_dichas])
+    return recuento
+
+def ordenarListaDescendiente(lista):
+    n = len(lista)
+    while n > 1:
+        for i in range(n-1):
+            if lista[i][1] < lista[i+1][1]:
+                lista[i], lista[i+1] = lista[i+1], lista[i]
+        n -= 1
+    return lista
+
+def segundoReporte():
+    listaOrdenada=ordenarListaDescendiente(obtenerRecuentoPalabras(registroParcicipantes,registroVoz))
+    print("A continuación se mostrará de orden descendente las personas de más palabras dijeron: \n")
+    for elemento in listaOrdenada:
+        print(elemento[0], "con", elemento[1],"palabras")
+        print("\n")
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Tercer reporte
+
+def tercerReporte():
+    
+    
+    return
+
+
+
 #------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-agregarApartados()
-agregarPuntos(agregarCodigoapartado(registroApartados))
-masApartados()
-agregarParticipantes()
-siguienteAccion(reconocimiento())
-seguirReconociendo()
+def ejecuciónPrograma():
+    system("cls")
+    print("TextToSpeech para Sesiones de Órganos Colegiados\n")
+    print("Se procede a realizar el registro de Agenda\n")
+    agregarApartados()
+    agregarPuntos(agregarCodigoapartado(registroApartados))
+    masApartados()
+    agregarParticipantes()
+    print("Apartados: \n")
+    siguienteAccion(reconocimiento())
+    seguirReconociendo()
+    print("Se proceden a realizar los reportes")
+    menuReportes()
+    seguirReportes()
+    print("\n El programa a finalizado su ejecución")
 
-print(registroVoz)
+#Se ejecuta el programa
+ejecuciónPrograma()
+
+
 
 
